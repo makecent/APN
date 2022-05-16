01dda697b6d48f82cb2dded75088c734aea3d294
louis <luchongkai96@gmail.com>
2022-05-16 11:26:15 +0800

    add

---
+++ configs/apn_r3dsony_32x4_10e_thumos14_rgb.py
@@ -0,0 +1,125 @@
+_base_ = [
+    './_base_/default_runtime.py'
+]
+
+# model settings
+model = dict(
+    type='APN',
+    backbone=dict(
+        type='ResNet3d_sony',
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='https://github.com/hassony2/kinetics_i3d_pytorch/raw/master/model/model_rgb.pth'),
+        modality='rgb'),
+    cls_head=dict(
+        type='APNHead',
+        num_classes=20,
+        in_channels=1024,
+        dropout_ratio=0.5))
+
+# input configuration
+clip_len = 32
+frame_interval = 4
+
+# dataset settings
+dataset_type = 'APNDataset'
+data_root = 'my_data/thumos14'
+
+data_train = (data_root + '/rawframes/train',
+              data_root + '/rawframes/val')
+data_val = data_root + '/rawframes/test'
+
+ann_file_train = (data_root + '/annotations/apn/apn_train.csv',
+                  data_root + '/annotations/apn/apn_val.csv')
+ann_file_val = data_root + '/annotations/apn/apn_test.csv'
+
+img_norm_cfg = dict(
+    mean=[128, 128, 128], std=[128, 128, 128], to_bgr=False)
+
+train_pipeline = [
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='LabelToOrdinal'),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'progression_label', 'class_label'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs', 'progression_label', 'class_label']),
+]
+val_pipeline = [
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='LabelToOrdinal'),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'progression_label', 'class_label'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs', 'progression_label', 'class_label']),
+]
+test_pipeline = [
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs']),
+]
+
+data = dict(
+    videos_per_gpu=10,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        ann_files=ann_file_train,
+        pipeline=train_pipeline,
+        data_prefixes=data_train,
+        filename_tmpl='img_{:05}.jpg',
+        modality='RGB'
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_files=ann_file_val,
+        pipeline=val_pipeline,
+        data_prefixes=data_val,
+        filename_tmpl='img_{:05}.jpg',
+        modality='RGB'
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_files=ann_file_val,
+        pipeline=test_pipeline,
+        data_prefixes=data_val,
+        filename_tmpl='img_{:05}.jpg',
+        modality='RGB',
+        untrimmed=True
+    ))
+
+# validation config
+evaluation = dict(metrics=['top_k_accuracy', 'MAE'], save_best='MAE', rule='less')
+
+# optimizer
+optimizer = dict(type='Adam', lr=1e-04)  # this lr is used for 2 gpus
+optimizer_config = dict(grad_clip=None)
+
+# learning policy
+lr_config = dict(policy='fixed')
+total_epochs = 10
+
+# output settings
+work_dir = './work_dirs/apn_r3dsony_32x4_10e_thumos14_rgb/'
+output_config = dict(out=f'{work_dir}/progressions.pkl')
+
+# testing config
+eval_config = dict(
+    metric_options=dict(
+        metrics='mAP',
+        mAP=dict(
+            search=dict(
+                min_e=60,
+                max_s=40,
+                min_L=60,
+                method='mse'),
+            nms=dict(iou_thr=0.4),
+            dump_detections=f'{work_dir}/detections.pkl',
+            dump_evaluation=f'{work_dir}/evaluation.json')))
