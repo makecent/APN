e79d3230ade8d24c09b8253d347a7de8990166b9
louis-4 <luchongkai96@gmail.com>
2022-07-08 21:18:10 +0800

    add x3d-s

---
+++ configs/apn_x3d-s_8x32_10e_aty13_video.py
@@ -0,0 +1,109 @@
+_base_ = [
+    './_base_/default_runtime.py'
+]
+
+# model settings
+model = dict(
+    type='APN',
+    backbone=dict(type='X3D', gamma_w=1, gamma_b=2.25, gamma_d=2.2),
+    cls_head=dict(
+        type='APNHead',
+        num_classes=200,
+        in_channels=432,
+        dropout_ratio=0.5))
+
+# input configuration
+clip_len = 8
+frame_interval = 32
+
+# dataset settings
+dataset_type = 'ActivityNet'
+data_root = 'my_data/activitynet'
+
+data_train = data_root + '/videos/train_resized'
+data_val = data_root + '/videos/val_resized'
+
+ann_file_train = data_root + '/annotations/apn/apn_aty_train_video.csv'
+ann_file_val = data_root + '/annotations/apn/apn_aty_val_video.csv'
+
+img_norm_cfg = dict(
+    mean=[128, 128, 128], std=[128, 128, 128], to_bgr=False)
+
+train_pipeline = [
+    dict(type='DecordInit'),
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='DecordDecode'),
+    dict(type='LabelToOrdinal'),
+    dict(type='Resize', scale=(160, 160), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs', 'progression_label', 'class_label'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs', 'progression_label', 'class_label']),
+]
+val_pipeline = [
+    dict(type='DecordInit'),
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(160, 160), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs']),
+]
+test_pipeline = [
+    dict(type='DecordInit'),
+    dict(type='FetchStackedFrames', clip_len=clip_len, frame_interval=frame_interval),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(160, 160), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='Collect', keys=['imgs'], meta_keys=()),
+    dict(type='ToTensor', keys=['imgs']),
+]
+
+data = dict(
+    videos_per_gpu=10,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        ann_files=ann_file_train,
+        pipeline=train_pipeline,
+        data_prefixes=data_train,
+        filename_tmpl='img_{:05}.jpg',
+        modality='Video'
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_files=ann_file_val,
+        pipeline=val_pipeline,
+        data_prefixes=data_val,
+        filename_tmpl='img_{:05}.jpg',
+        modality='Video',
+        untrimmed=True
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_files=ann_file_val,
+        pipeline=test_pipeline,
+        data_prefixes=data_val,
+        filename_tmpl='img_{:05}.jpg',
+        modality='Video',
+        untrimmed=True
+    ))
+
+# validation config
+evaluation = dict(metrics=['top_k_accuracy', 'MAE', 'mAP'], save_best='mAP', rule='greater')
+
+# optimizer
+optimizer = dict(type='Adam', lr=1e-4)
+optimizer_config = dict(grad_clip=dict(max_norm=20))
+# learning policy
+lr_config = dict(policy='Fixed',
+                 warmup='linear',
+                 warmup_ratio=0.01,
+                 warmup_iters=1,
+                 warmup_by_epoch=True)
+total_epochs = 10
+
+load_from = "https://download.openmmlab.com/mmaction/recognition/x3d/facebook/x3d_s_facebook_13x6x1_kinetics400_rgb_20201027-623825a0.pth"
