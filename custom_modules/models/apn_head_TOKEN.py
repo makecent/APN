a98618159aaf8fc85af3606b89c24798df4f01e5
louis-4 <luchongkai96@gmail.com>
2022-07-17 22:37:12 +0800

    ff

--- custom_modules/models/apn_head_TOKEN.py
+++ custom_modules/models/apn_head_TOKEN.py
@@ -8,7 +8,7 @@ from mmcv.cnn import kaiming_init
 from mmaction.models.builder import HEADS, build_loss
 
 @HEADS.register_module()
-class APNHead(nn.Module, metaclass=ABCMeta):
+class APNHead_TOKEN(nn.Module, metaclass=ABCMeta):
     """Regression head for APN.
 
     Args:
@@ -41,6 +41,12 @@ class APNHead(nn.Module, metaclass=ABCMeta):
         self.num_stages = num_stages
         self.dropout_ratio = dropout_ratio
 
+        self.reg_token = nn.Parameter(torch.zeros(1, 1, in_channels))
+        self.cls_attention1 = nn.MultiheadAttention(in_channels, num_heads=8, batch_first=True)
+        self.cls_attention2 = nn.MultiheadAttention(in_channels, num_heads=8, batch_first=True)
+        self.reg_attention1 = nn.MultiheadAttention(in_channels, num_heads=8, batch_first=True)
+        self.reg_attention2 = nn.MultiheadAttention(in_channels, num_heads=8, batch_first=True)
+
         self.avg_pool = nn.AdaptiveAvgPool3d((1, 1, 1)) if avg3d else nn.Identity()
 
         self.cls_fc = nn.Linear(self.in_channels, self.num_classes)
@@ -52,11 +58,28 @@ class APNHead(nn.Module, metaclass=ABCMeta):
         kaiming_init(self.cls_fc, a=0, nonlinearity='relu', distribution='uniform')
         kaiming_init(self.coral_fc, a=0, nonlinearity='relu', distribution='uniform')
         nn.init.constant_(self.coral_bias, 0)
+        nn.init.trunc_normal_(self.reg_token, std=0.02)
 
     def forward(self, x):
         x = self.avg_pool(x)
-        x = x.view(x.shape[0], -1)
-        x = F.dropout(x, p=self.dropout_ratio)
-        cls_score = self.cls_fc(x)
-        reg_score = self.coral_fc(x) + self.coral_bias
+        # x = x.view(x.shape[0], -1)
+        # x = F.dropout(x)
+        # cls_score = self.cls_fc(x)
+        # reg_score = self.coral_fc(x) + self.coral_bias
+
+        cls_x, _ = self.cls_attention1(query=x[:, :1, :], key=x, value=x, need_weights=False)
+        cls_x, _ = self.cls_attention2(query=cls_x[:, :1, :], key=cls_x, value=cls_x, need_weights=False)
+        cls_token = cls_x[:, 0, :]
+
+        reg_x = torch.cat((self.reg_token.expand(x.shape[0], -1, -1), x[:, 1:, :]), dim=1)
+        reg_x, _ = self.reg_attention1(query=reg_x[:, :1, :], key=reg_x, value=reg_x, need_weights=False)
+        reg_x, _ = self.reg_attention2(query=reg_x[:, :1, :], key=reg_x, value=reg_x, need_weights=False)
+        reg_token = reg_x[:, 0, :]
+
+        cls_token = F.dropout(cls_token, p=self.dropout_ratio)
+        reg_token = F.dropout(reg_token, p=self.dropout_ratio)
+
+        cls_score = self.cls_fc(cls_token)
+        reg_score = self.coral_fc(reg_token) + self.coral_bias
+
         return cls_score, reg_score
