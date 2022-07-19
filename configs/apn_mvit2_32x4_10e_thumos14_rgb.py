c2f480bc09070051916d41abdfc0516efacce3c9
louis-4 <luchongkai96@gmail.com>
2022-07-19 22:42:44 +0800

    ff

--- configs/apn_mvit2_32x4_10e_thumos14_rgb.py
+++ configs/apn_mvit2_32x4_10e_thumos14_rgb.py
@@ -12,8 +12,9 @@ model = dict(
         in_channels=768,
         dropout_ratio=0.5,
         avg3d=False),
-    blending=dict(type='BatchAugBlendingProg', blendings=(dict(type='MixupBlendingProg', num_classes=20, alpha=.8),
-                                                          dict(type='CutmixBlendingProg', num_classes=20, alpha=1.))),
+    # blending=dict(type='BatchAugBlendingProg', blendings=(dict(type='MixupBlendingProg', num_classes=20, alpha=.8),
+    #                                                       dict(type='CutmixBlendingProg', num_classes=20, alpha=1.))),
+    blending=dict(type='BatchAugBlendingProg', blendings=(dict(type='MixupBlendingProg', num_classes=20, alpha=.8),)),
 )
 
 # input configuration
