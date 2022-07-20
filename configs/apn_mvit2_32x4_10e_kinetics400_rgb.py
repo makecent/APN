36c2df8661e1789dfc832b30fb06ae82e9140a7e
louis-4 <luchongkai96@gmail.com>
2022-07-20 16:51:55 +0800

    ff

--- configs/apn_mvit2_32x4_10e_kinetics400_rgb.py
+++ configs/apn_mvit2_32x4_10e_kinetics400_rgb.py
@@ -70,7 +70,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs']),
 ]
 data = dict(
-    videos_per_gpu=8,
+    videos_per_gpu=2,
     workers_per_gpu=6,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
