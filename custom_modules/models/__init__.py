5053b3d5d413d2d07cb01221f6e1a79a771fd830
louis-4 <luchongkai96@gmail.com>
2022-08-20 18:01:51 +0800

    support f16 for recognizer3d.py

--- custom_modules/models/__init__.py
+++ custom_modules/models/__init__.py
@@ -6,6 +6,7 @@ from custom_modules.models.old_models.apn_global_local import *
 from custom_modules.models.old_models.apn_head_GL import *
 from .i3d_head import *
 from .apn_MULTI import *
+from .recognizer3d import *
 # from .apn_loss import *
 
 __all__ = [
