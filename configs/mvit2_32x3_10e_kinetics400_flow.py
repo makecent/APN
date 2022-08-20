c8c6e14915aa5c801a9ffd805abc4cef664b31d8
louis-4 <luchongkai96@gmail.com>
2022-08-20 17:55:09 +0800

    ff

--- configs/mvit2_32x3_10e_kinetics400_flow.py
+++ configs/mvit2_32x3_10e_kinetics400_flow.py
@@ -40,7 +40,7 @@ train_pipeline = [
     dict(type='RandomResizedCrop'),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
-    dict(type='pytorchvideo.RandAugment', magnitude=7, num_layers=4, prob=0.5),
+    # dict(type='pytorchvideo.RandAugment', magnitude=7, num_layers=4, prob=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=()),
