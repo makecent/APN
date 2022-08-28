f79092d2be07f288193e1c414902adc83766d2a0
louis-4 <luchongkai96@gmail.com>
2022-08-28 13:32:22 +0800

    ff

--- configs/mvit2_32x3_10e_kinetics400_flow.py
+++ configs/mvit2_32x3_10e_kinetics400_flow.py
@@ -119,4 +119,4 @@ optimizer_config = dict(grad_clip=dict(max_norm=1.0))
 # learning policy
 lr_config = dict(policy='CosineRestart', periods=[50, 25], restart_weights=[1, 0.5], min_lr_ratio=0.01)
 total_epochs = 75
-fp16 = dict()
+# fp16 = dict()
